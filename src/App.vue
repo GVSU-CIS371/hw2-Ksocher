0955f3edc1daf200f276622fa5e531bc06f53e45
ksocher <socherk@mail.gvsu.edu>
2025-04-06 16:01:51 -0400

    Copied code from previous hw

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <Beverage :isIced="beverageStore.currentTemp === 'Cold'" />
+    <Beverage 
+    :isIced="beverageStore.currentTemp === 'Cold'" 
+    :baseColor="selectedBaseColor"
+    :creamerColor="selectedCreamerColor"
+    :syrupColor="selectedSyrupColor"
+    />
     <ul>
       <li>
         <template v-for="temp in beverageStore.temps" :key="temp">
@@ -16,6 +21,48 @@
           </label>
         </template>
       </li>
+      <li>
+        <template v-for="base in bases" :key="base.id">
+          <label>
+            <input
+              type="radio"
+              name="base"
+              :id="`r${base.id}`"
+              :value="base.name"
+              v-model="currentBase"
+            />
+            {{ base.name }}
+          </label>
+        </template>
+      </li>
+      <li>
+        <template v-for="cream in creamers" :key="cream.id">
+          <label>
+            <input
+              type="radio"
+              name="creamer"
+              :id="`r${cream.id}`"
+              :value="cream.name"
+              v-model="currentCreamer"
+            />
+            {{ cream.name }}
+          </label>
+        </template>
+      </li>
+      <li>
+        <template v-for="syrup in syrups" :key="syrup.id">
+          <label>
+            <input
+              type="radio"
+              name="syrup"
+              :id="`r${syrup.id}`"
+              :value="syrup.name"
+              v-model="currentSyrup"
+            />
+            {{ syrup.name }}
+          </label>
+        </template>
+      </li>
     </ul>
     <input type="text" placeholder="Beverage Name" />
     <button>🍺 Make Beverage</button>
@@ -27,6 +74,25 @@
 import Beverage from "./components/Beverage.vue";
 import { useBeverageStore } from "./stores/beverageStore";
 const beverageStore = useBeverageStore();
+
+import { temps, currentTemp, bases, currentBase, creamers, currentCreamer, syrups, currentSyrup } from "./stores/beverage.ts";
+import { computed } from "vue";
+
+const selectedBaseColor = computed(() => {
+  const base = bases.value.find((b) => b.name === currentBase.value);
+  return base ? base.color : "transparent";
+});
+
+const selectedCreamerColor = computed(() => {
+  const creamer = creamers.value.find((c) => c.name === currentCreamer.value);
+  return creamer ? creamer.color : "transparent";
+});
+
+const selectedSyrupColor = computed(() => {
+  const syrup = syrups.value.find((s) => s.name === currentSyrup.value);
+  return syrup ? syrup.color : "transparent";
+});
+
 </script>
 
 <style lang="scss">
